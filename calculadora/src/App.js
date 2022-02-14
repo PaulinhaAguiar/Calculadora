58c4b839463e3a20c334263957ce063eb9b771ed
Ana Paula <anaaguiar20016@gmail.com>
2022-02-14 17:55:53 -0300

    Layot visor, inicio layout btns

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,8 +1,30 @@
+import { FaPercentage } from 'react-icons/fa';
+import { GrFormAdd, GrFormSubtract, GrFormClose } from 'react-icons/gr';
+import './styles.css';
 
 export default function App(){
   return(
-    <div>
-      <h1>OK</h1>
+    <div className="container"> 
+      <div className="calculadora">
+        <div className="visor">
+          <text>0</text>
+        </div>
+
+        <div className="calc-btns">
+          <div className='row-btn'>
+            <button>CLEAR</button>
+            <button>
+              <FaPercentage size={15} color='#FFF' />
+            </button>
+            <button>
+              <FaPercentage size={15} color='#FFF' />
+            </button>
+            <button>
+              <FaPercentage size={15} color='#FFF' />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
